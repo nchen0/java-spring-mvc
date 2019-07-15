a0672ae9a220d1123250c1f8b6cef0ab3abfc562
9che886 <nicky.chen0@gmail.com>
2019-07-15 15:59:42 -0400

    mvc

--- src/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,6 +1,8 @@
 package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -15,6 +17,21 @@ public class HelloWorldController {
     // need a controller method to process the HTML form after
     @RequestMapping("/processForm")
     public String processForm() {
+        return "helloworld";
+    }
+
+    // new controller method to read form data and
+    // add data to the model
+    @RequestMapping("/processFormTwo")
+    public String letsShout(Http request, Model model) {
+
+        // read the request parameter from the HTML form
+
+        // convert the data to all caps
+
+        // create the message
+
+        // add message to the model
         return "helloworld";
     }
 
